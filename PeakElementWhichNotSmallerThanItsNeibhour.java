3d04cf750c23f492d945faeb475d19dd6e9c5210
Alokmish11 <140834850+Alokmish11@users.noreply.github.com>
2024-05-12 16:13:25 +0530

    Add files via upload

---
+++ PeakElementWhichNotSmallerThanItsNeibhour.java
@@ -0,0 +1,25 @@
+ // input ={5,10,15,30,25}
+ // output=30,     30 neibhour is 15,and 25 both are smaller 30
+import java.util.*;
+ public class PeakElementWhichNotSmallerThanItsNeibhour {
+    static int peak(int arr[],int n){
+        if(n==1)
+            return 0;
+            if(arr[0]>=arr[1])
+                return 0;
+                if(arr[n-1]>=arr[n-2])
+                return n-1;
+            for(int i=1;i<n-1;i++){
+                if(arr[i]>=arr[i-1] && arr[i]>=arr[i+1] )
+                return i;
+            }
+            return 0;
+             }
+         public static void main(String[] args){
+            int n=6;
+            int arr[]={1, 3, 20, 4, 1, 0};
+            
+            System.out.println(peak(arr,n));
+         }
+            }
+
